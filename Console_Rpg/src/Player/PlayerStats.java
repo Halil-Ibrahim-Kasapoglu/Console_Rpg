0f6676160ab14384306e29890d5ecc2c941339cc
Halil-Ibrahim-Kasapoglu <36034222+Halil-Ibrahim-Kasapoglu@users.noreply.github.com>
2021-05-25 22:24:12 +0300

    refactor

---
+++ Console_Rpg/src/Player/PlayerStats.java
@@ -0,0 +1,26 @@
+package Player;
+
+public class PlayerStats {
+
+
+    //karakterin akil sagligini temsil eder
+    //+n ile -n arainsa bir deger alir
+    //yaptigimiz secimler etkiler
+    //cok fazla yada cok az olmasi olumle sonuclanabilir
+    private int sanity;
+
+
+    // karakterin zekasini belirler
+    // fazla zeka bezen acilmayacak seceneklerin acilmasina sebebiyet verebilir
+    // veya fazla zeka size ekstra yetenekler kazandirabilir
+    // dusuk zeka size kandirilmaniza sebebiyet verebilir
+    private int intelligence;
+
+
+    private int morality;
+
+
+    //yaptiginiz iyi secimlerle kotu secimlerin sonucu ortalama karma puaniniz
+    private int karma;
+
+}
